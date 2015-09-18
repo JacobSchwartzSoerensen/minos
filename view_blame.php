16bcd499bc3af940deb7ed4e58ffb5de260b5221
Jacob Sørensen <JacobSchwartzSoerensen@gmail.com>
2015-09-18 22:36:57 +0200

    Added check for blamed as well

--- view_blame.php
+++ view_blame.php
@@ -6,7 +6,7 @@ if(isset($_GET["create"]) && $_GET["create"] === "true"){
     $reason = $_POST["reason"];
     $blameid = createBlame($reason);
 
-    if(isset($_GET['blamer'])){
+    if(isset($_GET['blamer']) && isset($_GET['blamed'])){
         addBlamer($blameid, $_POST['blamer']);
         addBlamed($blameid, $_POST['blamed']);
     }
